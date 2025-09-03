510e2312fcd67a53725afcc7c3b127cc5a4b0d74
Ausianovich Kanstantsin <ausianovich.kanstantsin@gmail.com>
2025-09-03 17:12:52 +0300

    Initial Commit

---
+++ DailySpark/DailySparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailySparkApp.swift
+//  DailySpark
+//
+//  Created by Kanstantsin Ausianovich on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct DailySparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailySparkTests/DailySparkTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailySparkTests.swift
+//  DailySparkTests
+//
+//  Created by Kanstantsin Ausianovich on 03/09/2025.
+//
+
+import Testing
+@testable import DailySpark
+
+struct DailySparkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
